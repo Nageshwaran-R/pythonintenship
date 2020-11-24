eca5353378ed335791afe0f8407a386827c54bd5
Nageshwaran-R <74898664+Nageshwaran-R@users.noreply.github.com>
2020-11-24 12:26:54 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,27 @@
+ ## 1 convet the interger to the following instruction
+
+ a=b=c=20
+ a=a/10
+ b=b*20
+ c=c+60
+ print(a,b,c)
+ 
+ ## 2 Create a String variable of 5 characters  and replace the 3rd character with G
+ 1 method
+ a="POWER"
+ a=a[:2]+'G'+a[3:]  ##adding first two characters with 'G' and with the remaing character from 4th character
+ print(a)
+ 
+ 2 method
+ a="POWER"
+ a=a.replace('W','G') replace fuction is directly used
+ print(a)
+ 
+ ## 3 convert folat to int and int to float
+ 
+ a=89
+ b=5.89
+ a=float(a)   ## assigning a variable to float datatype
+ b=int(b)     ## assigning b variable to integer datatype
+ print(a,b)
+ 
