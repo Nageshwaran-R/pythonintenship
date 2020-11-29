75698d8738429c7392809f5de743e5e34e83ecb0
Nageshwaran-R <74898664+Nageshwaran-R@users.noreply.github.com>
2020-11-29 18:21:42 +0530

    Update day7.py

--- day7.py
+++ day7.py
@@ -7,8 +7,16 @@ task.py
  import module
  module.a.append("peach","green apple")
  print(module.a)
+	
+	
+## pandas package
+
+import pandas as pd
+import numpy as np
+s=pd.Series(["Nagesh","Ramesh","Suresh","Mukesh"])
+print(s)
  
- ## 3 random number
+## 3 random number
  
 from random import randint
 for _ in range(1):
