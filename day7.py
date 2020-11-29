91093d2e057e70c579b9bef03520ccbd3bd9433f
Nageshwaran-R <74898664+Nageshwaran-R@users.noreply.github.com>
2020-11-29 17:59:37 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,24 @@
+## 1 module
+
+module.py
+ a=["apple","Grape","pineapple","orange"]
+ 
+task.py
+ import module
+ module.a.append("peach","green apple")
+ print(module.a)
+ 
+ ## 3 random number
+ 
+from random import randint
+for _ in range(1):
+	value = randint(1,100)
+	print(value)
+
+
+
+## print the path using sys
+
+import sys
+print(sys.path)
+
