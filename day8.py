82df4dab25b2b4d18a0bf450b4c1f81b709a30e1
Nageshwaran-R <74898664+Nageshwaran-R@users.noreply.github.com>
2020-11-30 21:04:35 +0530

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,41 @@
+## 1 Try Exception
+
+try:
+ print(y)
+except SyntaxError:
+ print("Variable is not defined")
+
+
+x=3
+y=0
+try: 
+    result = x // y 
+    print("quotient ", result) 
+except ZeroDivisionError: 
+    print("Sorry ! You are dividing by zero ") 
+  
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+  
+  
+ try:
+  print(y)
+except:
+  print("variable is not defined")
+finally:
+  print("The error is finished")
+  
+ x = 109
+
+if (x % 10 = 0) 
+  raise ValueError("Only integers are allowed")
+
+    
+ ## 2 calcutor with try except
+ 
+ 
+
