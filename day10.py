b5e5665bc07004afdeff2b524f85c3e1719e7e72
Nageshwaran-R <74898664+Nageshwaran-R@users.noreply.github.com>
2020-12-02 15:44:51 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -9,7 +9,7 @@ if match != None:
 else:
     print("False")
     
-##  Python program that matches a word containing 'ab'.
+##  2  Python program that matches a word containing 'ab'.
 
 import re
 text = input("enter:")
@@ -20,4 +20,35 @@ if match != None:
 else:
     print("False")
     
-## a Python program to check for a number at the end of a word/sentence.
+## 3 Python program to check for a number at the end of a word/sentence.
+
+import re
+text = input("enter:")
+reg =re.search('[0-9]$',text)
+
+if reg != None:
+    print("True")
+else:
+    print("False")
+    
+
+## 4 a Python program to search the numbers (0-9) of length between 1 to 3 in a given string
+
+import re
+num=input("Enter : ")
+results = re.finditer(r"([0-9]{1,3})",num )
+print("Number of length 1 to 3")
+for n in results:
+     print(n.group(0))
+	 
+## 5 a Python program to match a string that contains only uppercase letters
+
+import re
+text = input("enter:")
+reg = '^[A-Z]*$'
+match = re.match(reg,text)
+if match != None:
+    print("True")
+else:
+    print("False")
+    
